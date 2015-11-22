7cff6535249e1396a94684e91bf2bec624d8374e
Ryan W. Moore <rwmoore07@gmail.com>
2015-11-22 17:42:32 -0500

    Remove dead code to load tournaments
    It always requires a password. Refactoring how tournaments are loaded.

--- tournament.js
+++ tournament.js
@@ -1,20 +1,7 @@
-function Tournament(proposedName, password, tournamentLoader, callback) {
-    if (! TournamentNameIsValid(proposedName)) {
-      throw new Error("Invalid tournament name");
-    } else {
-      this.tournament = tournamentLoader(proposedName, password, callback);
-    }
-}
-
-Tournament.prototype.isValid = function() {
-  return this.tournament != null;
-}
-
 function TournamentNameIsValid(proposedName) {
   return /^[a-z0-9]+$/gi.test(proposedName);
 }
 
 module.exports = {
-  TournamentNameIsValid: TournamentNameIsValid,
-  Tournament : Tournament
+  TournamentNameIsValid: TournamentNameIsValid
 };
